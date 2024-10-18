2920de26e4fa71f299b0a523b36bf9b6e0ec3b6e
Sanni Tuomisto <sanni.tuomisto@helsinki.fi>
2024-10-18 14:54:48 +0300

    Add task

--- tasks.py
+++ tasks.py
@@ -4,6 +4,6 @@ from invoke import task
 def data(ctx):
     ctx.run("python3 src/Luncher/lounaat.py", pty=True)
 
-# @task
-# def clustering(ctx):
-#     ctx.run("python3 src/Luncher/klusterointi.py", pty=True)
+@task
+def clustering(ctx):
+    ctx.run("python3 src/Luncher/klusterointi.py", pty=True)
